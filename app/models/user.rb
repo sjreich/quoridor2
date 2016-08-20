d9e7d14091e29266a84cb02eba8bf4cd7a3ffc78
Joe Reich <jreich@covermymeds.com>
2016-08-20 12:47:32 -0400

    introduce rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   def games
     Game.where(player1: id)
-      .or(Game.where(player2: id))
-      .or(Game.where(player3: id))
-      .or(Game.where(player4: id))
+        .or(Game.where(player2: id))
+        .or(Game.where(player3: id))
+        .or(Game.where(player4: id))
   end
 
   has_secure_password
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810041023) do
-
-  create_table "games", force: :cascade do |t|
-    t.integer  "player1_id", null: false
-    t.integer  "player2_id", null: false
-    t.integer  "player3_id"
-    t.integer  "player4_id"
-    t.integer  "winner"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["player1_id"], name: "index_games_on_player1_id"
-    t.index ["player2_id"], name: "index_games_on_player2_id"
-    t.index ["player3_id"], name: "index_games_on_player3_id"
-    t.index ["player4_id"], name: "index_games_on_player4_id"
+ActiveRecord::Schema.define(version: 20_160_810_041_023) do
+  create_table 'games', force: :cascade do |t|
+    t.integer  'player1_id', null: false
+    t.integer  'player2_id', null: false
+    t.integer  'player3_id'
+    t.integer  'player4_id'
+    t.integer  'winner'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['player1_id'], name: 'index_games_on_player1_id'
+    t.index ['player2_id'], name: 'index_games_on_player2_id'
+    t.index ['player3_id'], name: 'index_games_on_player3_id'
+    t.index ['player4_id'], name: 'index_games_on_player4_id'
   end
 
   create_table 'moves', force: :cascade do |t|
@@ -33,17 +32,16 @@ ActiveRecord::Schema.define(version: 20160810041023) do
     t.integer  'variety'
     t.integer  'x'
     t.integer  'y'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+    t.datetime 'created_at',   null: false
+    t.datetime 'updated_at',   null: false
     t.index ['game_id'], name: 'index_moves_on_game_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "username",        null: false
-    t.string   "password_digest", null: false
-    t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+  create_table 'users', force: :cascade do |t|
+    t.string   'username',        null: false
+    t.string   'password_digest', null: false
+    t.string   'email'
+    t.datetime 'created_at',      null: false
+    t.datetime 'updated_at',      null: false
   end
-
 end
