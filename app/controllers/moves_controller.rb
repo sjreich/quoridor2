9fdaf53e2f754239af4f3ee80858cdcf50dd473f
Joe Reich <jreich@covermymeds.com>
2016-09-26 00:33:50 -0400

    getting the moves controller right

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -2,11 +2,15 @@ class MovesController < ApplicationController
   before_action :fetch_game
 
   def create
-    @game.moves.create
+    @game.moves.create(move_params)
     redirect_to @game
   end
 
   private
+
+  def move_params
+    params.require(:move).permit(:player, :variety, :x, :y)
+  end
 
   def fetch_game
     @game = Game.find(params[:game_id])
