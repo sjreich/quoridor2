9ceb6cadd7b06f60898b105898bd6812735641b2
Joe Reich <jreich@covermymeds.com>
2016-10-30 16:01:23 -0400

    use factory girl for moves

--- spec/factories.rb
+++ spec/factories.rb
@@ -9,4 +9,11 @@ FactoryGirl.define do
     association :player1, factory: :user
     association :player2, factory: :user
   end
+
+  factory :move do
+    variety :translation
+    player 1
+    x 1
+    y 1
+  end
 end
--- spec/models/move_spec.rb
+++ spec/models/move_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Move, type: :model do
   describe 'ordinal attribute auto-increments' do
-    subject { m = Move.create(game: game, variety: :translation, player: 1, x: 1, y: 1).reload.ordinal }
+    subject { create(:move, game: game).reload.ordinal }
     let(:game) { create :game }
 
     context 'when this is the first move of the game' do
@@ -12,7 +12,7 @@ RSpec.describe Move, type: :model do
     context 'when the game has previously existing moves' do
       let(:previous_move_count) { rand(2..7) }
       before do
-        previous_move_count.times { Move.create(game: game, variety: :translation, player: 1, x: 1, y: 1) }
+        previous_move_count.times { create(:move, game: game) }
       end
 
       it { should eq previous_move_count + 1 }
@@ -22,12 +22,12 @@ RSpec.describe Move, type: :model do
   describe 'ordinal is unique within the game' do
     subject { new_move.save }
 
-    let(:new_move) { Move.new(game: current_game, ordinal: new_ordinal, variety: :translation, player: 1, x: 1, y: 1) }
+    let(:new_move) { build :move, game: current_game, ordinal: new_ordinal }
     let!(:current_game) { create :game }
     let(:other_game) { create :game }
 
     context 'when CURRENT game has a 23rd move' do
-      before { Move.create(game: current_game, ordinal: 23, variety: :translation, player: 1, x: 1, y: 1) }
+      before { create :move, game: current_game, ordinal: 23 }
 
       context 'when new ordinal is 23' do
         let(:new_ordinal) { 23 }
@@ -41,7 +41,7 @@ RSpec.describe Move, type: :model do
     end
 
     context 'when DIFFERENT game has a 23rd move' do
-      before { Move.create(game: other_game, ordinal: 23) }
+      before { create :move, game: other_game, ordinal: 23 }
 
       context 'when new ordinal is 23' do
         let(:new_ordinal) { 23 }
